e96828a7a5dec8fc701f3af2fa8a1ade392e919d
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-04-12 12:08:12 +0200

    Base Classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import img_dice from "./assets/images/icon-dice.svg"
+import img_divider_large from "./assets/images/pattern-divider-desktop.svg"
+import img_divider_small from "./assets/images/pattern-divider-mobile.svg"
+
 function App() {
 	return (
 		<>
-			
+			<main>
+				<div className="container">
+					<div className="card | text-center">
+						<h1 className="card__heading | letter-spacing-loose">Test Heading</h1>
+						<p>Test Advice</p>
+						<img src={img_divider_large} alt="A divider for better visualization" />
+						<button className="button | round" data-type="dice">
+							<img src={img_dice} alt="A dice meant to reroll the advice" />
+						</button>
+					</div>
+				</div>
+			</main>
 		</>
   )
 }
