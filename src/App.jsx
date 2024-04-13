7f0b2fc2aa7f92da07b5ca225fd5b1d4f36c7c2b
UltimateToaster <polikarpovzakhar@gmail.com>
2024-04-13 18:51:43 +0200

    Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import img_divider_large from "./assets/images/pattern-divider-desktop.svg"
 import img_divider_small from "./assets/images/pattern-divider-mobile.svg"
 import { useState, useEffect } from "react";
 
+function ResponsiveImage () {
+	return (
+		<picture className="divider">
+			<source
+				media="(max-width: 425px)"
+				srcSet={img_divider_small}
+				sizes="425px"
+			/>
+			<source
+				srcSet={img_divider_large}
+				sizes="1280px"
+			/>
+			<img src={img_divider_large} />
+		</picture>
+	);
+ }
 
 function App() {
 	const [advice, setAdvice] = useState("");
@@ -11,7 +27,6 @@ function App() {
 		fetch("https://api.adviceslip.com/advice")
 			.then(res => res.json())
 			.then(data => {
-				console.log(data.slip);
 				setAdvice(data.slip);
 			});
 	}
@@ -25,10 +40,11 @@ function App() {
 		<>
 			<main>
 				<div className="container">
-					<div className="card | text-center">
+					<div className="card | text-center items-center">
 						<h1 className="card__heading | letter-spacing-loose">advice #{advice.id}</h1>
-						<p>{advice.advice}</p>
-						<img src={img_divider_large} alt="A divider for better visualization" />
+						<p>“{advice.advice}”</p>
+						{/* <img className="divider" src={img_divider_large} srcSet={`${img_divider_large}, ${img_divider_small}`} alt="A divider for better visualization" /> */}
+						<ResponsiveImage />
 						<button className="button | round" data-type="dice" onClick={getNewAdvice}>
 							<img src={img_dice} alt="A dice meant to reroll the advice" />
 						</button>
